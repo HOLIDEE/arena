4bd326b21cf6e4fe3d4607f2a52dff6342ae9815
eneyret <manu@holidee.fr>
2024-01-13 17:26:49 +0100

    Maj

---
+++ tilesets/fire.tsx
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="fire" tilewidth="32" tileheight="32" tilecount="440" columns="22">
+ <image source="fire.png" width="704" height="640"/>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="31" duration="250"/>
+   <frame tileid="33" duration="250"/>
+   <frame tileid="35" duration="250"/>
+   <frame tileid="37" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <animation>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="31" duration="250"/>
+   <frame tileid="33" duration="250"/>
+   <frame tileid="35" duration="250"/>
+   <frame tileid="37" duration="250"/>
+   <frame tileid="23" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="27" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="45">
+  <animation>
+   <frame tileid="45" duration="250"/>
+   <frame tileid="47" duration="250"/>
+   <frame tileid="49" duration="250"/>
+   <frame tileid="51" duration="250"/>
+   <frame tileid="53" duration="250"/>
+   <frame tileid="55" duration="250"/>
+   <frame tileid="57" duration="250"/>
+   <frame tileid="59" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="51">
+  <animation>
+   <frame tileid="51" duration="250"/>
+   <frame tileid="53" duration="250"/>
+   <frame tileid="55" duration="250"/>
+   <frame tileid="57" duration="250"/>
+   <frame tileid="59" duration="250"/>
+   <frame tileid="45" duration="250"/>
+   <frame tileid="47" duration="250"/>
+   <frame tileid="49" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="118">
+  <animation>
+   <frame tileid="118" duration="250"/>
+   <frame tileid="120" duration="250"/>
+   <frame tileid="122" duration="250"/>
+   <frame tileid="124" duration="250"/>
+   <frame tileid="126" duration="250"/>
+   <frame tileid="128" duration="250"/>
+   <frame tileid="130" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="119">
+  <animation>
+   <frame tileid="119" duration="250"/>
+   <frame tileid="121" duration="250"/>
+   <frame tileid="123" duration="250"/>
+   <frame tileid="125" duration="250"/>
+   <frame tileid="127" duration="250"/>
+   <frame tileid="129" duration="250"/>
+   <frame tileid="131" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="140">
+  <animation>
+   <frame tileid="140" duration="250"/>
+   <frame tileid="142" duration="250"/>
+   <frame tileid="144" duration="250"/>
+   <frame tileid="146" duration="250"/>
+   <frame tileid="148" duration="250"/>
+   <frame tileid="150" duration="250"/>
+   <frame tileid="152" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="141">
+  <animation>
+   <frame tileid="141" duration="250"/>
+   <frame tileid="143" duration="250"/>
+   <frame tileid="145" duration="250"/>
+   <frame tileid="147" duration="250"/>
+   <frame tileid="149" duration="250"/>
+   <frame tileid="151" duration="250"/>
+   <frame tileid="153" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="162">
+  <animation>
+   <frame tileid="162" duration="250"/>
+   <frame tileid="164" duration="250"/>
+   <frame tileid="166" duration="250"/>
+   <frame tileid="168" duration="250"/>
+   <frame tileid="170" duration="250"/>
+   <frame tileid="172" duration="250"/>
+   <frame tileid="174" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="163">
+  <animation>
+   <frame tileid="163" duration="250"/>
+   <frame tileid="165" duration="250"/>
+   <frame tileid="167" duration="250"/>
+   <frame tileid="169" duration="250"/>
+   <frame tileid="171" duration="250"/>
+   <frame tileid="173" duration="250"/>
+   <frame tileid="175" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
